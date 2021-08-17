df420d14ec4a9ffcbe0af621c9ee54555e9dd598
Juan <juan.bigorra@sennder.com>
2021-08-17 17:55:29 +0200

    Finish iteration two

--- tests/rpg_combat_kata/test_character.py
+++ tests/rpg_combat_kata/test_character.py
@@ -4,21 +4,30 @@ import pytest
 
 
 class Character:
-    def __init__(self):
+    def __init__(self, level=1):
         self._alive = True
-        self._level = 1
+        self._level = level
         self._health = 1000
 
     pass
 
-    def attack(self, character: Character, damage: float):
-        character._health -= damage
-        if character._health < 0:
-            character._health = 0
-            character._alive = False
+    def attack(self, target: Character, damage: float):
+        if target == self:
+            return
+
+        if target._level - self._level >= 5:
+            damage *= 0.5
+        elif self._level - target._level >= 5:
+            damage *= 1.5
+
+        target._health -= damage
+
+        if target._health < 0:
+            target._health = 0
+            target._alive = False
 
     def heal(self, character: Character, heal_amount: float):
-        if not character._alive:
+        if character != self or not character._alive:
             return
 
         character._health += heal_amount
@@ -28,6 +37,8 @@ class Character:
 
 
 class TestCharacter:
+
+    # Iteration one
 
     # 1. All Characters, when created, have:
     #   - Health, starting at 1000
@@ -81,3 +92,44 @@ class TestCharacter:
 
         assert character2._health == expected_health
         assert character2._alive == expected_alive_status
+
+    # Iteration two
+
+    # A Character cannot Deal Damage to itself.
+
+    def test_character_cannot_damage_itself(self):
+        character = Character()
+
+        character.attack(character, 300)
+
+        assert character._health == 1000
+
+    # A Character can only Heal itself.
+    def test_character_can_only_heal_itself(self):
+        character1 = Character()
+        character2 = Character()
+        character1.attack(character2, 100)
+
+        character1.heal(character2, 50)
+
+        assert character2._health == 900
+
+    # If the target is 5 or more Levels above the attacker, Damage is reduced by 50%
+    @pytest.mark.parametrize("target_level", [6, 10])
+    def test_reduce_damage_when_target_is_5_or_more_levels_above_attacker(self, target_level):
+        character = Character()
+        target = Character(level=target_level)
+
+        character.attack(target, 100)
+
+        assert target._health == 950
+
+    # If the target is 5 or more Levels below the attacker, Damage is increased by 50%
+    @pytest.mark.parametrize("character_level", [6, 10])
+    def test_increase_damage_when_character_is_5_or_more_levels_above_target(self, character_level):
+        character = Character(level=character_level)
+        target = Character()
+
+        character.attack(target, 100)
+
+        assert target._health == 850
